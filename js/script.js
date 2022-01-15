40251b69c2817d633b58c357a1db3f9e78714bb4
Suvadeep <maityamalendu18@gmail.com>
2022-01-15 20:51:32 +0530

    5days bootacamp done

---
+++ js/script.js
@@ -0,0 +1,57 @@
+const form = document.querySelector("form")
+const search = document.querySelector("input")
+const searchResult = document.querySelector(".results")
+
+
+const apiUrl = "https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch="
+
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    var searchvalue = search.value;
+
+    if(searchvalue === ""){
+        alert("Search is empty");
+       
+    }
+
+    else{
+        getResult(searchvalue);
+    }
+})
+
+
+async function getResult(searchval)
+{
+    const response = await fetch(apiUrl+searchval);
+    const result = await response.json();
+    // console.log(result);
+    if (result.query.search.length == 0) {
+        alert("Not found!!");
+        search.value = "";
+    }
+    else{
+        displayResult(result);
+        search.value = "";
+    }
+}
+
+function displayResult(results)
+{
+    var printValue = "";
+
+    results.query.search.forEach((e)=>{
+        var resultUrl = `https://en.wikipedia.org/?curid=${e.pageid}`
+        printValue += 
+
+        `
+       
+            <h2>${e.title}</h2>
+            <a href=${resultUrl} target="__blank">Know More</a>
+            <p>${e.snippet}</p>
+            <h3>Time: ${e.timestamp}</h3>
+        `
+        searchResult.innerHTML = printValue
+    })
+
+}
